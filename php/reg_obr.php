210d161d9826047d441570767daeda6079341b76
Chizhikov <57985484+SergioChizhikov@users.noreply.github.com>
2022-04-25 10:25:01 +0300

    Добавил подключение к БД

--- php/reg_obr.php
+++ php/reg_obr.php
@@ -1,9 +1,17 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
+$mysqli = mysqli_connect("localhost", "oefnbvtr_0207", "12345", "oefnbvtr_0207");
+
+if ($mysqli == false) {
+  print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+} else {
+  print("Соединение установлено успешно");
+}
+
 $name = $_POST['name'];
 $lastname = $_POST['lastname'];
 $email = $_POST['email'];
 $pass = $_POST['pass'];
 
-echo "Имя: $name<br>Фамилия: $lastname<br>Email: $email<br>Пароль: $pass";
+// echo "Имя: $name<br>Фамилия: $lastname<br>Email: $email<br>Пароль: $pass";
