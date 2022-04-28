3dff958793b607c52a9905a0d1d5cb8ef93f8a6c
Chizhikov <57985484+SergioChizhikov@users.noreply.github.com>
2022-04-28 10:30:54 +0300

    Работают изменния по кнопкам

--- lk.php
+++ lk.php
@@ -49,13 +49,13 @@
 		<!-- Строка с именем -->
 		<p>Имя: <span><?php echo $_SESSION['name'] ?></span>
 			<span class="edit-btn">[Изменить]</span>
-			<span class="save-btn" hidden>[Сохранить]</span>
+			<span class="save-btn" hidden data-item="name">[Сохранить]</span>
 			<span class="cancel-btn" hidden>[Отменить]</span>
 		</p>
 		<!-- Строка с фамилией -->
 		<p>Фамилия: <span><?php echo $_SESSION['lastname'] ?></span>
 			<span class="edit-btn">[Изменить]</span>
-			<span class="save-btn" hidden>[Сохранить]</span>
+			<span class="save-btn" hidden data-item="lastname">[Сохранить]</span>
 			<span class="cancel-btn" hidden>[Отменить]</span>
 		</p>
 
@@ -81,11 +81,29 @@
 
 			});
 			//Работаю с кнопкой save
-			//save_button
+			save_buttons[i].addEventListener("click", async () => {
+				let newInputValue = edit_buttons[i].previousElementSibling.firstElementChild.value;
+				edit_buttons[i].previousElementSibling.innerText = newInputValue;
+				save_buttons[i].hidden = true;
+				cancel_buttons[i].hidden = true;
+				edit_buttons[i].hidden = false;
+
+				let FormData = new FormData();
+				FormData.append("value", newInputValue);
+				FormData.append("item", save_buttons[i].dataset.item);
+
+				let response = await fetch("php/lk_obr.php", {
+					method: "POST",
+					body: FormData,
+				})
+			})
 
 			//Работаю с кнопкой cancel
 			cancel_buttons[i].addEventListener("click", function() {
-
+				edit_buttons[i].previousElementSibling.innerText = inputValue;
+				save_buttons[i].hidden = true;
+				cancel_buttons[i].hidden = true;
+				edit_buttons[i].hidden = false;
 			})
 
 		}
