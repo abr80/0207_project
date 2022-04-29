c2dbeb489afda0a7570732c24cb9d55dd23de73c
Chizhikov <57985484+SergioChizhikov@users.noreply.github.com>
2022-04-29 10:09:20 +0300

    Рабочий запрос с бд и обновл. сессии

--- php/lk_obr.php
+++ php/lk_obr.php
@@ -7,9 +7,12 @@ $mysqli = mysqli_connect("localhost", "oefnbvtr_0207", "12345", "oefnbvtr_0207")
 if ($mysqli == false) {
   print("error");
 } else {
-$inputValue = $_POST['value'];
-$item = $_POST['item'];
-$id = $_SESSION['id'];
-$mysqli->query("UPDATE `users` SET `name`=[value-2],`lastname`=[value-3],`email`=[value-4],`pass`=[value-5] WHERE 1");
+  $inputValue = $_POST['value'];
+  $item = $_POST['item'];
+  
+  $id = $_SESSION['id'];
 
+  $mysqli->query("UPDATE `users` SET `$item` = '$inputValue' WHERE `id` = '$id'");
+
+  $_SESSION[$item] = $inputValue;
 }
